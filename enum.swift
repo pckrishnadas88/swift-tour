149c8d960be6eb752169b930ed56b7e167837c64
pckrishnadas88 <pckrishnadas88@gmail.com>
2016-11-08 20:39:49 +0530

    Completed protocols.

---
+++ enum.swift
@@ -0,0 +1,44 @@
+enum Rank: Int {
+case ace = 1
+case two, three, four, five, six, seven, eight, nine, ten
+case jack, queen, king
+	func simpleDescription() -> String {
+		switch self {
+		case .ace:
+			return "ace"
+		case .jack:
+			return "jack"
+		case .queen:
+			return "queen"
+		case .king:
+			return "king"
+		default:
+			return String(self.rawValue)
+
+		}
+	}
+}
+let ace = Rank.ace
+let aceRawValue = ace.rawValue
+print("Ace rawValue is \(aceRawValue)")
+
+
+
+enum Suit {
+case spades, hearts, diamonds, clubs
+	func simpleDescription() -> String {
+		switch self {
+		case .spades:
+			return "spades"
+		case .hearts:
+			return "hearts"
+		case .diamonds:
+			return "diamonds"
+		case .clubs:
+			return "clubs"
+		}
+	}
+}
+let hearts = Suit.hearts
+let heartsDescription = hearts.simpleDescription()
+print("Hearts description \(heartsDescription)")
---
+++ protocol.swift
@@ -0,0 +1,26 @@
+protocol ExampleProtocol {
+  var simpleDescription: String { get }
+  mutating func adjust()
+}
+
+class simpleClass: ExampleProtocol {
+  var simpleDescription: String = "A very simple class"
+  var anotherProperty: Int = 69015
+  func adjust() {
+    simpleDescription += " Now 100% adjusted"
+  }
+}
+var a = simpleClass()
+a.adjust()
+print(a.simpleDescription)
+
+struct SimpleStructure: ExampleProtocol {
+  var simpleDescription: String = "A simple structure"
+  mutating func adjust() {
+    simpleDescription += " (adjusted)"
+  }
+}
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+print(bDescription)
