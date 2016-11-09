60e0bfb77d05624ce702c198d199bbec3fdd63d0
krishnadas <krishnadas>
2016-11-09 15:55:47 +0530

    Completed generics

---
+++ generics.swift
@@ -0,0 +1,13 @@
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool {
+    where T.Iterator.Element: Equatable, T.Iterator.Element == U.Iterator.Element {
+        for lhsItem in lhs {
+            for rhsItem in rhs {
+                if lhsItem == rhsItem {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+}
+anyCommonElements([1, 2, 3], [3])
